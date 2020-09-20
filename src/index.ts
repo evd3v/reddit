4dc447b7b76c49bd430fc28000a455a9e20794a6
evd3v <evd3v@yandex.ru>
2020-09-20 14:32:24 +0300

    init commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { MikroORM } from "@mikro-orm/core"
+import microConfig from "./mikro-orm.config"
+import express from "express"
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig)
+  await orm.getMigrator().up()
+
+  const app = express()
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000")
+  })
+}
+
+main().catch((err) => {
+  console.error(err)
+})
